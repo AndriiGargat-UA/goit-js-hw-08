38bb7d012cda9030f2fe0dfe4880a3f49d117a3d
Andrii Gargat <andreysax1312@gmail.com>
2022-10-26 12:17:57 +0300

    all complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
+const galleryRef = document.querySelector('.gallery');
+const galleryMarcup = createGalleryItemMarcup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMarcup);
+
+function createGalleryItemMarcup() {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+};
+
+galleryRef.addEventListener('click', onGalleryRefClick);
+let lightboxGallery = new SimpleLightbox('.gallery a', { animationSlide: true, captionsData: "alt", captionDelay: 250 });
+
+function onGalleryRefClick(event) {
+    event.preventDefault();
+    const isGalleryRef = event.target.classList.contains('gallery__image');
+    if (!isGalleryRef) {        
+        return
+    }
+}
 console.log(galleryItems);
+
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from "lodash.throttle";
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
+
+const STORAGE_KEY = "feedback-form-state";
+let formData = {};
+
+saveData();
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));    
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    console.log(localStorage.getItem(STORAGE_KEY)); 
+    localStorage.removeItem(STORAGE_KEY);    
+}
+
+function saveData() {
+    const savedFormDada = localStorage.getItem(STORAGE_KEY);
+    const parsedFormData = JSON.parse(savedFormDada);
+
+if (parsedFormData) {
+      formData = parsedFormData;
+      refs.input.value = formData.email || '';
+      refs.textarea.value = formData.message || '';
+    }
+};
